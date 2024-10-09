d820125c258efeb86baa2e3518e1327bcc866205
Rasulbekovich01 <iskandarovmuhammad450@gmail.com>
2024-10-09 16:35:52 +0500

    first commit

---
+++ conf/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from forms.views import create_user_info_view
+
+urlpatterns = [
+    path('ckedidor/', include())
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('form/', include('forms.urls', namespace="form")),
+    path('', include('news.urls', namespace="news")),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
